orchardup/docker-postgresql

commit bd66030e1cba32c4b7221691bdfebe93b408de5d

Updated everything to ubuntu:14.04 and postgres 9.3

Dockerfile
@@ -1,15 +1,16 @@
-FROM stackbrew/ubuntu:12.04
+FROM stackbrew/ubuntu:14.04
 MAINTAINER Ben Firshman "ben@orchardup.com"
 
 RUN locale-gen en_US.UTF-8
-RUN apt-get -qq update && LC_ALL=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get install -y -q postgresql-9.1 postgresql-contrib-9.1 postgresql-9.1-postgis libpq-dev sudo
+RUN apt-get -qq update && LC_ALL=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get install -y -q postgresql-9.3 postgresql-contrib-9.3 postgresql-9.3-postgis-2.1 libpq-dev sudo
 
 # /etc/ssl/private can't be accessed from within container for some reason
 # (@andrewgodwin says it's something AUFS related)
 RUN mkdir /etc/ssl/private-copy; mv /etc/ssl/private/* /etc/ssl/private-copy/; rm -r /etc/ssl/private; mv /etc/ssl/private-copy /etc/ssl/private; chmod -R 0700 /etc/ssl/private; chown -R postgres /etc/ssl/private
 
-ADD postgresql.conf /etc/postgresql/9.1/main/postgresql.conf
-ADD pg_hba.conf /etc/postgresql/9.1/main/pg_hba.conf
+ADD postgresql.conf /etc/postgresql/9.3/main/postgresql.conf
+ADD pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf
+RUN chown postgres:postgres /etc/postgresql/9.3/main/*.conf
 ADD run /usr/local/bin/run
 RUN chmod +x /usr/local/bin/run
 

run
@@ -6,16 +6,16 @@ POSTGRESQL_PASS=${POSTGRESQL_PASS:-"docker"}
 POSTGRESQL_DB=${POSTGRESQL_DB:-"docker"}
 POSTGRESQL_TEMPLATE=${POSTGRESQL_TEMPLATE:-"DEFAULT"}
 
-POSTGRESQL_BIN=/usr/lib/postgresql/9.1/bin/postgres
-POSTGRESQL_CONFIG_FILE=/etc/postgresql/9.1/main/postgresql.conf
-POSTGRESQL_DATA=/var/lib/postgresql/9.1/main
+POSTGRESQL_BIN=/usr/lib/postgresql/9.3/bin/postgres
+POSTGRESQL_CONFIG_FILE=/etc/postgresql/9.3/main/postgresql.conf
+POSTGRESQL_DATA=/var/lib/postgresql/9.3/main
 
 POSTGRESQL_SINGLE="sudo -u postgres $POSTGRESQL_BIN --single --config-file=$POSTGRESQL_CONFIG_FILE"
 
 if [ ! -d $POSTGRESQL_DATA ]; then
     mkdir -p $POSTGRESQL_DATA
     chown -R postgres:postgres $POSTGRESQL_DATA
-    sudo -u postgres /usr/lib/postgresql/9.1/bin/initdb -D $POSTGRESQL_DATA -E 'UTF-8'
+    sudo -u postgres /usr/lib/postgresql/9.3/bin/initdb -D $POSTGRESQL_DATA -E 'UTF-8'
     ln -s /etc/ssl/certs/ssl-cert-snakeoil.pem $POSTGRESQL_DATA/server.crt
     ln -s /etc/ssl/private/ssl-cert-snakeoil.key $POSTGRESQL_DATA/server.key
 fi

commit 955ac7787440aca56d32906e7338fd4ca8d9cb27

Fix template encoding
Regressed in bd66030e1cba32c4b7221691bdfebe93b408de5d

Closes #13.

Dockerfile
@@ -2,7 +2,9 @@ FROM ubuntu:14.04
 MAINTAINER Ben Firshman "ben@orchardup.com"
 
 RUN locale-gen en_US.UTF-8
-RUN apt-get -qq update && LC_ALL=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get install -y -q postgresql-9.3 postgresql-contrib-9.3 postgresql-9.3-postgis-2.1 libpq-dev sudo
+RUN update-locale LANG=en_US.UTF-8
+
+RUN apt-get -qq update && DEBIAN_FRONTEND=noninteractive apt-get install -y -q postgresql-9.3 postgresql-contrib-9.3 postgresql-9.3-postgis-2.1 libpq-dev sudo
 
 # /etc/ssl/private can't be accessed from within container for some reason
 # (@andrewgodwin says it's something AUFS related)